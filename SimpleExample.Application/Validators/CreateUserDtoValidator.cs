8ff49397a0ac201a7d546d577c07e691736d4290
KhaledAlkilani <khaledkelany@gmail.com>
2026-02-08 09:58:35 +0200

    Test failing tests in CI

---
+++ SimpleExample.Application/Validators/CreateUserDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using SimpleExample.Application.DTOs;
+
+namespace SimpleExample.Application.Validators
+{
+    public class CreateUserDtoValidator : AbstractValidator<CreateUserDto>
+    {
+        public CreateUserDtoValidator()
+        {
+            RuleFor(x => x.FirstName).NotEmpty();
+            RuleFor(x => x.LastName).NotEmpty();
+            RuleFor(x => x.Email).NotEmpty().EmailAddress();
+        }
+    }
+}
---
+++ SimpleExample.Tests/Validators/CreateUserDtoValidatorTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation.TestHelper;
+using SimpleExample.Application.DTOs;
+using SimpleExample.Application.Validators;
+
+namespace SimpleExample.Tests.Validators;
+
+public class CreateUserDtoValidatorTests
+{
+    private readonly CreateUserDtoValidator _validator;
+
+    public CreateUserDtoValidatorTests()
+    {
+        _validator = new CreateUserDtoValidator();
+    }
+
+    [Fact]
+    public void Should_Have_Error_When_FirstName_Is_Empty()
+    {
+        var dto = new CreateUserDto
+        {
+            FirstName = "",
+            LastName = "Meikäläinen",
+            Email = "test@test.com"
+        };
+
+        var result = _validator.TestValidate(dto);
+
+        // intentionally wrong expectation for the assignment step
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+}
